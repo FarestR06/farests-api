7c9f6a4670f47a0dba4157047bee091be8db3fb2
FarestR06 <jacobjdolunt@gmail.com>
2024-02-25 16:40:39 -0500

    Initial commit

---
+++ src/main/java/com/farestr06/api/FarestsAPIDataGenerator.java
@@ -0,0 +1,11 @@
+package com.farestr06.api;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FarestsAPIDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
