af7b986e7a601c399f37ca12f0d1ddf867c9e99f
farestr06 <jacobjdolunt@gmail.com>
2024-06-21 17:42:52 -0400

    Removed examples to fix an issue

--- src/main/java/com/farestr06/api/FarestsAPI.java
+++ src/main/java/com/farestr06/api/FarestsAPI.java
@@ -1,12 +1,7 @@
 package com.farestr06.api;
 
-import com.farestr06.api.example.ExampleBlock;
-import com.farestr06.api.example.ExampleSound;
-import com.farestr06.api.example.item.ExampleItemGroup;
-import com.farestr06.api.util.RegistryHelper;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,10 +9,8 @@ public class FarestsAPI implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "fr06-api";
 
     public static final Logger LOGGER = LoggerFactory.getLogger("fr06-api");
-	public static final RegistryHelper HELPER = new RegistryHelper(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -26,11 +19,5 @@ public class FarestsAPI implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("farest");
-
-		if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
-			ExampleBlock.register();
-			ExampleSound.register();
-			ExampleItemGroup.register();
-		}
 	}
 }
