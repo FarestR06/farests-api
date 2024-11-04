be40ba5823634d4487202c313a5d2141c18b769d
farestr06 <jacobjdolunt@gmail.com>
2024-11-04 14:12:03 -0500

    Remove pointless registry failures

--- src/main/java/com/farestr06/api/util/exception/GameObject.java
+++
@@ -1,9 +0,0 @@
-package com.farestr06.api.util.exception;
-
-import net.minecraft.util.Identifier;
-import org.jetbrains.annotations.ApiStatus;
-
-@Deprecated(forRemoval = true)
-@ApiStatus.Internal
-public record GameObject(String registryName, Identifier location) {
-}
