127a533cd2fbb45e0ffed351c765d2e5f8ead763
farestr06 <jacobjdolunt@gmail.com>
2024-11-05 15:57:07 -0500

    Fix SoundHelper

--- src/main/java/com/farestr06/api/sound/SoundHelper.java
+++ src/main/java/com/farestr06/api/sound/SoundHelper.java
@@ -7,10 +7,16 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 public final class SoundHelper {
-    public static RegistryEntry.Reference<SoundEvent> makeEvent(Identifier id) {
-        return Registry.registerReference(Registries.SOUND_EVENT, id, SoundEvent.of(id));
+    public static SoundEvent makeEvent(Identifier id) {
+        return Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(id));
+
+    }public static SoundEvent makeEvent(Identifier registryId, Identifier soundId) {
+        return Registry.register(Registries.SOUND_EVENT, registryId, SoundEvent.of(soundId));
     }
     public static RegistryEntry.Reference<SoundEvent> makeReferenceEvent(Identifier id) {
         return Registry.registerReference(Registries.SOUND_EVENT, id, SoundEvent.of(id));
+    }
+    public static RegistryEntry.Reference<SoundEvent> makeReferenceEvent(Identifier registryId, Identifier soundId) {
+        return Registry.registerReference(Registries.SOUND_EVENT, registryId, SoundEvent.of(soundId));
     }
 }
